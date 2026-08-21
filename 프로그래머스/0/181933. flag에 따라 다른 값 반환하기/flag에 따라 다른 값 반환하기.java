7038153dbd25cf4d34655220162a34efa866fd19
SEO JINGYO <oygnijoes0209@gmail.com>
2026-08-21 12:14:38 +0900

    Delete 프로그래머스/0 directory

--- 프로그래머스/0/181933. flag에 따라 다른 값 반환하기/flag에 따라 다른 값 반환하기.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public int solution(int a, int b, boolean flag) {
-        if (flag == true) {
-            return a + b;
-        } else {
-            return a - b;
-        }
-    }
-}
