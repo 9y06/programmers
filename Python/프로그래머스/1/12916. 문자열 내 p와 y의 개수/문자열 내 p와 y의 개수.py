ccd309c7bc64d18288804934539ff8316810f09c
SEO JINGYO <oygnijoes0209@gmail.com>
2026-08-29 16:02:20 +0900

    [level 1] Title: 문자열 내 p와 y의 개수, Time: 0.00 ms, Memory: 11.6 MB -BaekjoonHub

---
+++ Python/프로그래머스/1/12916. 문자열 내 p와 y의 개수/문자열 내 p와 y의 개수.py
@@ -0,0 +1,7 @@
+def solution(s):
+    if s.lower().count("p") == s.lower().count("y"):
+        return True
+    elif s.lower().count("p") == 0 and s.lower().count("y") == 0:
+        return True
+    else:
+        return False
