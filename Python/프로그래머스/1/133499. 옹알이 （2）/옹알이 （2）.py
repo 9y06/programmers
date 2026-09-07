fc06a62c7f74456d64ce175cbc6c1cfc6f2545ce
SEO JINGYO <oygnijoes0209@gmail.com>
2026-09-07 11:17:44 +0900

    [level 1] Title: 옹알이 (2), Time: 0.07 ms, Memory: 11.3 MB -BaekjoonHub

---
+++ Python/프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.py
@@ -0,0 +1,13 @@
+def solution(babbling):
+    answer = 0
+    baby = ["aya", "ye", "woo", "ma"]
+    for i in babbling:
+        for j in baby:
+            if j * 2 in i:
+                break
+        else:
+            for j in baby:
+                i = i.replace(j, " ")
+            if i.strip() == "":
+                answer += 1
+    return answer
