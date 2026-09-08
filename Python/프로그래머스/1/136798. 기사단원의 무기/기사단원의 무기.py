5126fea3155e01eafb78ae0ed15f32ba3bc2c2de
SEO JINGYO <oygnijoes0209@gmail.com>
2026-09-08 20:15:04 +0900

    [level 1] Title: 기사단원의 무기, Time: 621.48 ms, Memory: 12.4 MB -BaekjoonHub

--- Python/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
+++ Python/프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -1,15 +1,17 @@
 def solution(number, limit, power):
+    li = []
     answer = 0
-    m = []
     for i in range(1, number+1):
         cnt = 0
         for j in range(1, int(i**0.5)+1):
             if i % j == 0:
                 cnt += 1
-                if j != i // j:
+                if i % (i // j) == 0 and j != i // j:
                     cnt += 1
-        if cnt > limit:
+        li.append(cnt)
+    for i in li:
+        if i > limit:
             answer += power
         else:
-            answer += cnt
+            answer += i
     return answer
