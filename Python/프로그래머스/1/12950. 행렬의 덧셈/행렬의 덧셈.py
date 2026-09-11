e2520b5a97ec36d27964af47e424a49c343d733c
SEO JINGYO <oygnijoes0209@gmail.com>
2026-09-11 13:55:46 +0900

    [level 1] Title: 행렬의 덧셈, Time: 7.97 ms, Memory: 22.4 MB -BaekjoonHub

--- Python/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ Python/프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,10 +1,8 @@
 def solution(arr1, arr2):
-    answer2 = []
-    arr_len = len(arr1)
-    x = len(arr1[0])
-    for i in range(arr_len):
-        answer1 = []
-        for j in range(x):
-            answer1.append(arr1[i][j] + arr2[i][j])
-        answer2.append(answer1)
-    return answer2
+    answer = []
+    for i in range(len(arr1)):
+        a = []
+        for j in range(len(arr1[0])):
+            a.append(arr1[i][j]+arr2[i][j])
+        answer.append(a)
+    return answer
