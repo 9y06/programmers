004fc2469288fb1126924cc592352a3471afd9aa
SEO JINGYO <oygnijoes0209@gmail.com>
2026-09-11 11:07:37 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- Python/프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
+++ Python/프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.py
@@ -1,7 +1,7 @@
-import math
-
 def solution(s):
+    answer = ''
     if len(s) % 2 == 0:
-        return s[len(s)//2-1:len(s)//2+1]
+        answer += s[len(s)//2-1:len(s)//2+1]
     else:
-        return s[math.ceil(len(s)//2)]
+        answer += s[len(s)//2]
+    return answer
