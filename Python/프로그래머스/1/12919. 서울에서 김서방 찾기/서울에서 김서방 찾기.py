6bbb53848ee9f87d8d596a82a66e7929fca905f8
SEO JINGYO <oygnijoes0209@gmail.com>
2026-09-11 10:01:39 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 11.6 MB -BaekjoonHub

--- Python/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
+++ Python/프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.py
@@ -1,7 +1,5 @@
 def solution(seoul):
-    answer = ''
-    for i in seoul:
-        if i == "Kim":
-            location = seoul.index("Kim")
-    answer = f"김서방은 {location}에 있다"
-    return answer
+    for i in range(len(seoul)):
+        if seoul[i] == "Kim":
+            answer = f'김서방은 {i}에 있다'
+            return answer
