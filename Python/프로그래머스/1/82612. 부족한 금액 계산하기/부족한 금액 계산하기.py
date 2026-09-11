6c663b03f008acdec542bb9932d5a2fd40cce3d7
SEO JINGYO <oygnijoes0209@gmail.com>
2026-09-11 11:27:43 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.10 ms, Memory: 11.6 MB -BaekjoonHub

--- Python/프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
+++ Python/프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -1,9 +1,7 @@
 def solution(price, money, count):
-    answer = 0
-    cost = 0
     for i in range(1, count+1):
-        cost += price * i
-    if cost <= money:
+        money -= price * i
+    if money > 0:
         return 0
     else:
-        return abs(cost - money)
+        return money * -1
