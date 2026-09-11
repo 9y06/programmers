7d9cc933ae500d824f15736082f56567d30b4e2f
SEO JINGYO <oygnijoes0209@gmail.com>
2026-09-11 11:17:01 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 13.36 ms, Memory: 11.3 MB -BaekjoonHub

--- Python/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ Python/프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,13 +1,12 @@
 def solution(left, right):
     answer = 0
-    result = 0
     for i in range(left, right+1):
+        cnt = 0
         for j in range(1, i+1):
             if i % j == 0:
-                answer += 1
-        if answer % 2 == 0:
-            result += i
+                cnt += 1
+        if cnt % 2 == 0:
+            answer += i
         else:
-            result -= i
-        answer = 0
-    return result
+            answer -= i
+    return answer
