aee3e8a13c43b96094693687353b168e38ea9b5f
SEO JINGYO <oygnijoes0209@gmail.com>
2026-09-11 11:33:55 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.00 ms, Memory: 11.5 MB -BaekjoonHub

--- Python/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ Python/프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -2,7 +2,4 @@ def solution(s):
     if len(s) == 4 or len(s) == 6:
         if s.isdigit():
             return True
-        else:
-            return False
-    else:
-        return False
+    return False
