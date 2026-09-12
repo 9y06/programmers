471cb63dc087f8d8e211ede7031b34b4978d6e4c
SEO JINGYO <oygnijoes0209@gmail.com>
2026-09-12 11:50:58 +0900

    [level 1] Title: 명예의 전당 (1), Time: 1.50 ms, Memory: 11.1 MB -BaekjoonHub

--- Python/프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
+++ Python/프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.py
@@ -2,9 +2,8 @@ def solution(k, score):
     answer = []
     li = []
     for i in score:
-        answer.append(i)
-        answer.sort(reverse=True)
-        if len(answer) >= k:
-            del answer[k:]
-        li.append(min(answer))
-    return li
+        li.append(i)
+        li.sort(reverse=True)
+        myeong = li[:k]
+        answer.append(myeong[-1])
+    return answer
