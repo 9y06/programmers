db5121ff49a65f4fe3fb78b7218c0b5a4934f11f
SEO JINGYO <oygnijoes0209@gmail.com>
2026-09-12 12:12:37 +0900

    [level 1] Title: 폰켓몬, Time: 0.54 ms, Memory: 12.3 MB -BaekjoonHub

--- Python/프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ Python/프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,12 +1,4 @@
 def solution(nums):
-    answer = []
-    n = len(nums) // 2
-    for i in nums:
-        if i in answer:
-            pass
-        else:
-            if len(answer) >= n:
-                return len(answer)
-            else:
-                answer.append(i)
-    return len(answer)
+    can = int(len(nums)/2)
+    phone = len(set(nums))
+    return min(phone, can)
